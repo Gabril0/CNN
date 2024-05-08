03a2cf14ebab993abd5f5b38b5d7e0e2acfea61a
Gabril0 <gabrielgcrepaldi@gmail.com>
2024-05-08 11:39:53 -0300

    Added some data aug transformations

--- Model/test.py
+++ Model/test.py
@@ -1,11 +1,16 @@
 import torch
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, ToPILImage
+from torchvision.utils import save_image
+from TrainingDataArrangement import arrange_data
+from torchvision.transforms import v2
+
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 import CustomDataset
-from TrainingDataArrangement import arrange_data
 
+#defining device
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -16,7 +21,6 @@ device = (
 print(f"Using {device} device")
 
 #getting path references
-#training used only the roi folders
 testing_folders = [
     r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\testing\tumor\patch\640x640\1009009\01-roi",
     r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\testing\tumor\patch\640x640\1009010x1000902\01-roi",
@@ -47,29 +51,64 @@ training_folders = [
     r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\training\tumor\patch\640x640\1009023\02-non_roi"
 ]
 
-arrange_data(r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\appended_folder\testing",testing_folders)
-arrange_data(r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\appended_folder\training",training_folders)
+updated_training_folders = []
+updated_testing_folders = []
 
-training_dataset = CustomDataset.CustomImageDataset(training_path, mask_path, transform=ToTensor())
-test_dataset = CustomDataset.CustomImageDataset(test_path, test_mask_path, transform=ToTensor())
+for training_folder, testing_folder in zip(training_folders, testing_folders):
+    original_training_folder = training_folder + "\\01-original"
+    mask_training_folder = training_folder + "\\02-mask"
+    updated_training_folders.extend([original_training_folder, mask_training_folder])
+
+    original_testing_folder = testing_folder + "\\01-original"
+    mask_testing_folder = testing_folder + "\\02-mask"
+    updated_testing_folders.extend([original_testing_folder, mask_testing_folder])
+
+training_path = r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\appended_folder\training"
+test_path = r"D:\Estudos\IC\DataSets\H&E-stained oral squamous cell carcinoma histological images dataset\H&E-stained oral squamous cell carcinoma histological images dataset\appended_folder\testing"
+
+arrange_data(test_path,updated_testing_folders)
+arrange_data(training_path,updated_training_folders)
+
+#data augmentation
+augmentation_transform = v2.Compose([
+    v2.ToPILImage(),
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.RandomVerticalFlip(p=0.25),
+    v2.RandomRotation(degrees=45),
+    v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+    v2.RandomResizedCrop(224, scale=(0.8, 1.0)),
+    v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]), #loading image to tensor
+])
 
 
+print("Getting the images")
+training_dataset = CustomDataset.CustomImageDataset(training_path + "\\images", training_path + "\\masks", transform=augmentation_transform)
+test_dataset = CustomDataset.CustomImageDataset(test_path + "\\images", test_path + "\\masks", transform=augmentation_transform)
 
+print("Converting images to tensors")
 train_dataloader = DataLoader(training_dataset, batch_size=64, shuffle=True)
 test_dataloader = DataLoader(test_dataset , batch_size=64, shuffle=True)
 
-for train_batch, mask_batch in train_dataloader:
-    train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
 
-for train_batch, mask_batch in test_dataloader:
-    train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
+print(len(train_dataloader.dataset))
 
-#display image and label.
-train_features, train_labels = next(iter(train_dataloader))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
-img = train_features[0].squeeze()
-label = train_labels[0]
-plt.imshow(img, cmap="gray")
-plt.show()
-print(f"Label: {label}")
+
+
+
+
+
+
+
+
+
+
+
+
+# print("Loading images to gpu tensors")
+# for train_batch, mask_batch in train_dataloader:
+#     train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
+
+# for train_batch, mask_batch in test_dataloader:
+#     train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
+
+
