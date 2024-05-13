98b603eced5c0567ee3f2ae23c7dd8fcd8782e6e
Gabril0 <gabrielgcrepaldi@gmail.com>
2024-05-13 09:59:05 -0300

    Added UNET model

--- Model/CustomDataset.py
+++ Model/CustomDataset.py
@@ -9,19 +9,23 @@ class CustomImageDataset(Dataset):
         self.train_img_files = os.listdir(train_img_dir)
         self.mask_img_files = os.listdir(mask_img_dir)
         self.transform = transform
+        self.num_images = 0
 
-    def __len__(self):
+    def __len__(self): 
         return min(len(self.train_img_files), len(self.mask_img_files))
 
     def __getitem__(self, idx):
+        self.num_images = self.num_images + 1
+        print(f"Getting item at index: {idx}, total number of images: {self.num_images}")
         train_img_path = os.path.join(self.train_img_dir, self.train_img_files[idx])
         mask_img_path = os.path.join(self.mask_img_dir, self.mask_img_files[idx])
 
-        train_image = read_image(train_img_path)
+        train_image = read_image(train_img_path) #getting the image
         mask_image = read_image(mask_img_path)
 
         if self.transform:
-            train_image = self.transform(train_image)
+            train_image = self.transform(train_image) #applying the effects
             mask_image = self.transform(mask_image)
 
         return train_image, mask_image
+
--- Model/test.py
+++ Model/test.py
@@ -4,11 +4,15 @@ from torchvision.transforms import ToTensor, ToPILImage
 from torchvision.utils import save_image
 from TrainingDataArrangement import arrange_data
 from torchvision.transforms import v2
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 import os
 import numpy as np
 import CustomDataset
+from UNetModel import UNet
 
 #defining device
 device = (
@@ -78,6 +82,7 @@ augmentation_transform = v2.Compose([
     v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
     v2.RandomResizedCrop(224, scale=(0.8, 1.0)),
     v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]), #loading image to tensor
+    #v2.Normalize()
 ])
 
 
@@ -88,27 +93,54 @@ test_dataset = CustomDataset.CustomImageDataset(test_path + "\\images", test_pat
 print("Converting images to tensors")
 train_dataloader = DataLoader(training_dataset, batch_size=64, shuffle=True)
 test_dataloader = DataLoader(test_dataset , batch_size=64, shuffle=True)
+print("Loading images to gpu tensors")
+for train_batch, mask_batch in train_dataloader:
+    train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
 
+for train_batch, mask_batch in test_dataloader:
+    train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
 
-print(len(train_dataloader.dataset))
+channels = 4
+classes = 2
+model = UNet(channels, classes)
+input_data = torch.randn(1, channels, 256, 256)
+output = model(input_data)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+model.to(device)
 
+num_epochs = 10
 
+#training
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    
+    for images, masks in tqdm(train_dataloader, desc=f'Epoch {epoch + 1}/{num_epochs}', leave=False):
+        images, masks = images.to(device), masks.to(device)
+        
+        optimizer.zero_grad()
+        
+        outputs = model(images)
+        
+        loss = criterion(outputs, masks)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    
+    print(f"Epoch {epoch + 1} Loss: {running_loss / len(train_dataloader)}")
 
+model.eval()
+test_loss = 0.0
+with torch.no_grad():
+    for images, masks in tqdm(test_dataloader, desc='Testing', leave=False):
+        images, masks = images.to(device), masks.to(device)
+        
+        outputs = model(images)
+        
+        test_loss += criterion(outputs, masks).item()
 
-
-
-
-
-
-
-
-# print("Loading images to gpu tensors")
-# for train_batch, mask_batch in train_dataloader:
-#     train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
-
-# for train_batch, mask_batch in test_dataloader:
-#     train_batch, mask_batch = train_batch.to(device), mask_batch.to(device)
-
-
+print(f"Average Test Loss: {test_loss / len(test_dataloader)}")
